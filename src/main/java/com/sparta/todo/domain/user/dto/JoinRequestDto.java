15d0315ab135b40f43a08ea25152deeb00132c96
하승 <dlgktmd0@naver.com>
2024-10-16 17:16:25 +0900

    Feat - 유저 CRUD
    유저 회원가입 및 로그인 구현

---
+++ src/main/java/com/sparta/todo/domain/user/dto/JoinRequestDto.java
@@ -0,0 +1,19 @@
+package com.sparta.todo.domain.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+
+@Getter
+public class JoinRequestDto {
+    @Email
+    private String email;
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$",
+            message = "비밀번호는 최소 8자리 이상, 영문, 숫자, 특수문자를 1자 이상 포함되어야 합니다.")
+    @NotBlank
+    private String password;
+    @NotBlank
+    private String userName;
+    private String adminToken = "";
+}
---
+++ src/main/java/com/sparta/todo/domain/user/dto/LoginRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.todo.domain.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class LoginRequestDto {
+    @NotBlank(message = "이메일을 입력해야합니다.")
+    private String email;
+    @NotBlank(message = "password 입력해주세요.")
+    private String password;
+}
